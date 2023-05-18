60e07f123e7320179bfa4c93c8068eb4c08413c7
PicciJr <piccijunior2319@gmail.com>
2023-05-18 20:59:34 +0200

    add all ui elements in the home page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,35 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import DatePicker from './components/DatePicker.vue'
+import Dropdown from './components/Dropdown.vue'
 </script>
 
 <template>
-  <h1 class="text-red-500 text-2xl">Sigan</h1>
+  <div class="max-w-2xl mx-auto border p-4 bg-green-50 rounded-lg mt-24 flex flex-col gap-4">
+    <!-- Datepicker -->
+    <DatePicker />
+    <!-- Filter -->
+    <Dropdown button-text="Filter type" />
+    <!-- Terms and conditions -->
+    <div class="flex gap-3 items-center">
+      <input
+        id="default-checkbox"
+        type="checkbox"
+        class="w-4 h-4 text-gray-700 bg-gray-100 border-gray-300 rounded focus:ring-gray-500 dark:focus:ring-gray-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+      />
+      <label
+        for="default-checkbox"
+        class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+        >I accept the terms and conditions</label
+      >
+    </div>
+
+    <!-- Submit button -->
+    <button
+      type="button"
+      class="text-white bg-green-700 hover:bg-greeb-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800 w-fit"
+    >
+      Get results
+    </button>
+  </div>
 </template>
---
+++ src/components/Dropdown.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="w-44">
+    <button
+      class="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center"
+      type="button"
+      @click="toggleDropdown"
+    >
+      {{ buttonText }}
+      <svg
+        class="w-4 h-4 ml-2"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M19 9l-7 7-7-7"
+        ></path>
+      </svg>
+    </button>
+    <div
+      :class="[
+        'bg-white text-base rounded shadow-sm mt-2 py-2 px-4',
+        { hidden: !isDrowdownVisible }
+      ]"
+      id="dropdown"
+    >
+      <ul class="p-2 space-y-3" aria-labelledby="dropdown">
+        <li v-for="(item, index) in items" :key="index">
+          <button>{{ item }}</button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, ref } from 'vue'
+
+defineProps({
+  items: {
+    type: Array,
+    default: () => []
+  },
+  buttonText: {
+    type: String,
+    default: 'Dropdown button'
+  },
+  items: {
+    type: Array,
+    default: () => ['item1', 'item2']
+  }
+})
+
+const isDrowdownVisible = ref(false)
+
+const toggleDropdown = () => {
+  isDrowdownVisible.value = !isDrowdownVisible.value
+}
+</script>
