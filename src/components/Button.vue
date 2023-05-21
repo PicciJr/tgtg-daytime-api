b39375d852096aea3447efec3ec1eedd86c73b7a
PicciJr <piccijunior2319@gmail.com>
2023-05-21 10:17:49 +0200

    add disable control to button

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -3,10 +3,10 @@
     type="button"
     :class="[
       'text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800 w-fit',
-      { 'cursor-not-allowed': isLoading }
+      { 'cursor-not-allowed': isLoading || isDisabled }
     ]"
     @click="emit('click')"
-    :disabled="isLoading"
+    :disabled="isLoading || isDisabled"
   >
     <svg
       v-if="isLoading"
@@ -41,6 +41,10 @@ defineProps({
     default: 'Button'
   },
   isLoading: {
+    type: Boolean,
+    default: false
+  },
+  isDisabled: {
     type: Boolean,
     default: false
   }
